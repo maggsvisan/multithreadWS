55a6d0241fbbe8c6fbea802caca7a79bb47ddd8d
Margarita Villarreal <maggsvisan@users.noreply.github.com>
2017-05-11 00:19:10 -0500

    FINAL!
    Execution time reduces according to the quantity of threads.  
    (More threads, less workload)

--- final.cpp
+++ final.cpp
@@ -13,88 +13,159 @@
 #include <string>
 #include <queue>
 #include <time.h>
-
-
 #include <pthread.h>
 #include <stdio.h>
-#define NUM_THREADS     5
+
+#define NUM_THREADS 4
 
 using namespace cv;
 using namespace std;
 
+int c = 0; //num de objetos locales por thread
+pthread_mutex_t mutexc;
+clock_t start,end;
+
+struct myStruct{
+  int thRow;
+  int index;
+  int end;
+  int thId;
+  int r;
+  int g;
+  int b;
+}thStruct[NUM_THREADS];
+
+Mat image= imread("imagen2.png");
+Mat newreg(image.rows,image.cols,CV_8UC3,Scalar(255,255,255));
+int baseIntervalo;
+ 
+void *myThreadRoutine(void *thStr){
 
 queue<Point> coords; //guarda los vecinos de objetos
 queue<Point> temp;
 Point p, ptemp;
-int c = 0; //num de objetos
-int xp=0;
-int yp=0;
-int r=0;
-int g=0;
-int b=0;
-int bb=0;
-int gg=0;
-int rr=0;
 
-//generate random colors
-void colors(int &bb, int &gg, int &rr){
+  int xp=0;
+  int yp=0;
 
-  bb= theRNG().uniform(0,255);
-  gg= theRNG().uniform(0,255);
-  rr= theRNG().uniform(0,255);
+  struct myStruct *th =(struct myStruct *)thStr;
 
-}
+  int rowNum = th->thRow;
+  int tid = th->thId;
+  int inicio = th->index;
+  int final = th->end;
+  int rr = th->r;
+  int gg = th->g;
+  int bb = th->b;
 
- void *PrintHello(void *threadid)
- {
-    long tid;
-    tid = (long)threadid;
-    printf("Hello World! It's me, thread #%ld!\n", tid);
-    pthread_exit(NULL);
- }
+pthread_mutex_lock(&mutexc);
+for (int j=inicio; j <= final ; j++)
+{//las columnas son el eje de las x
+    for (int i=1; i<rowNum; i++)
+      {//las filas son el eje de las y  
+        //if (tid ==9)       
+            // cout<< j<<","<<i<< endl;  
+           if ((image.at<Vec3b>(i,j)== Vec3b(0,0,0) && (newreg.at<Vec3b>(i,j)== Vec3b(255,255,255))))// de la semilla
+           {
+            p.x=j;
+            p.y=i;
+            coords.push(p); //se guardanlas coordenadas
+            c++; //contador de objetos
+            //creamos rgb randoms
+            
+            newreg.at<Vec3b>(i, j)= Vec3b(bb,gg,rr); //pintamos pixel
+            while(!coords.empty())
+            {  
+              ptemp=coords.front();            
+              xp= ptemp.x;
+              yp= ptemp.y;
+              //y , x   
+              if ( (image.at<Vec3b>(yp,xp+1)== Vec3b(0,0,0)) && (newreg.at<Vec3b>(yp,xp+1)== Vec3b(255,255,255)))//right
+              {
+                newreg.at<Vec3b>(yp, xp+1)= Vec3b(bb,rr,gg);//pintamos el punto 
+                ptemp.x= xp+1;
+                ptemp.y= yp;
+                coords.push(ptemp);
+              }
+              if ( (image.at<Vec3b>(yp+1,xp)== Vec3b(0,0,0)) && (newreg.at<Vec3b>(yp+1, xp) == Vec3b(255,255,255))) //up
+              {
+                newreg.at<Vec3b>(yp+1,xp)= Vec3b(bb,rr,gg);//pintamos el punto
+                ptemp.x= xp;
+                ptemp.y= yp+1;
+                coords.push(ptemp);
+              }
+              if ( (image.at<Vec3b>(yp,xp-1)== Vec3b(0,0,0)) && (newreg.at<Vec3b>(yp, xp-1) == Vec3b(255,255,255))) //left
+              {
+                newreg.at<Vec3b>(yp,xp-1)= Vec3b(bb,rr,gg);//pintamos el punto
+                ptemp.x= xp-1;
+                ptemp.y= yp;
+                coords.push(ptemp);
+              }
+              if ( (image.at<Vec3b>(yp-1,xp)== Vec3b(0,0,0)) && (newreg.at<Vec3b>(yp-1, xp) == Vec3b(255,255,255))) //Down
+              {
+                newreg.at<Vec3b>(yp-1,xp)= Vec3b(bb,rr,gg);//pintamos el punto
+                ptemp.x= xp;
+                ptemp.y= yp-1;
+                coords.push(ptemp);
+              }
+              coords.pop();
+            }
+          }  
+      }                 
+   }
+   pthread_mutex_unlock(&mutexc);
+}   
 
-
-void watershed(){
-
+void watershed(Mat image, Mat newreg){
+    int row=0;
+    row= image.rows;
+    //erode(image, image, Mat(), Point(-1,-1),2);
     pthread_t threads[NUM_THREADS];
     int rc;
     long t;
+    void *status;
 
-    for(t=0; t<NUM_THREADS; t++){
-       printf("In main: creating thread %ld\n", t);
-       rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+    cout<<"entra"<< endl;
     
-    }
-
-    
-    
-}
-
- int main (int argc, char *argv[])
- {
-    clock_t start,end;
-    Mat image= imread("bin.jpg");
-    imshow("Original",image);
-    Mat newreg(image.rows,image.cols,CV_8UC3,Scalar(255,255,255));
-     
+    baseIntervalo= row / NUM_THREADS; //intervalo por thread, base
     start=clock();
 
-   // watershed(image, newreg);
-   
-    watershed();
-
+    for(t=0; t<NUM_THREADS; t++){
+      thStruct[t].thRow = baseIntervalo;
+      thStruct[t].thId = t;
+      thStruct[t].index = t*baseIntervalo+1;
+      thStruct[t].r = rand()%256;
+      thStruct[t].g = rand()%256;
+      thStruct[t].b = rand()%256;
+      
+      if(t==NUM_THREADS-1){
+        thStruct[t].end = image.cols;
+      }
+      else{
+        thStruct[t].end = (t+1)*baseIntervalo;
+      }
+      pthread_create(&threads[t], NULL , myThreadRoutine,&thStruct[t]);
+    }
+    for(t=0; t<NUM_THREADS; t++){
+      pthread_join(threads[t], &status);
+    }
     end=clock();
 
-     printf("time: %ld ", end-start);
+}
 
-
-    imshow("newreg segmentado", newreg);
-
-    cout<<"el numero de objetos es "<< c <<endl; //aqui vamos a poner el total de la suma de cada thread
-    waitKey(0);
-
-    /* Last thing that main() should do */
-    pthread_exit(NULL);
-    
-    return 0;
- }
+int main (int argc, char *argv[])
+{
+/*
+    cout<<"cols"<< image.cols<<endl;
+    cout<<"rows"<< image.rows<<endl;
+*/
+  imshow("Original",image);
+  watershed(image, newreg);
+  printf("time: %ld ", end-start);
+  imshow("newreg segmentado", newreg);
+  cout<<"el numero de objetos es "<< c <<endl; //aqui vamos a poner el total de la suma de cada thread
+  waitKey(0);
+  pthread_mutex_destroy(&mutexc);
+  /* Last thing that main() should do */
+  return 0;
+}
